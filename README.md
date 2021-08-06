a6024f06a0ca2452a04a77063ffa0d436c30e28f
savva.genchevskiy <savva.genchevskiy@ab-soft.net>
2021-08-06 16:45:38 +0300

    initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+## 1. Application and Monitoring Setup:
+ - `docker-compose buid`
+ - `docker-compose up -d`
+
+Now application is available by: http://127.0.0.1:8080/
+
+Expected Output:
+``` 
+Hello World!
+```
+
+## 2. Running
---
+++ src/gatling/scala/com/example/simulations/TestSimulation.scala
@@ -0,0 +1,32 @@
+package com.example.simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+import scala.concurrent.duration.DurationInt
+import scala.language.postfixOps
+
+class TestSimulation extends Simulation {
+
+  var httpConf: HttpProtocolBuilder = http.baseUrl("http://localhost:8080")
+    .acceptCharsetHeader("utf-8")
+    .disableWarmUp
+
+  val testScn: ScenarioBuilder = scenario("get hello world")
+    .exec(
+      http("").get("/")
+        .check(status.is(200))
+        .check(bodyString.is("Hello World!"))
+    )
+
+  setUp(testScn.inject(constantUsersPerSec(20) during(2.minutes))
+    .throttle(
+      reachRps(10) in(10 seconds),
+      holdFor(2 minutes)
+    ))
+    .maxDuration(5.minutes)
+    .protocols(httpConf)
+
+}
